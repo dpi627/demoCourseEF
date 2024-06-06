a96a12920ef52cd6b9eb2880fcb6f3a0c81513e8
brian_li <brian.li@sgs.com>
2024-06-06 11:29:29 +0800

    調整組態設定檔
    fet: 移除敏感資訊
    fet: 建立 Developmnet 組態與 secret.json
    fet: 修改預設 webapi 測試取得連線字串
    fet: launchSettings.json 新增組態

--- demoCourseEF/Controllers/WeatherForecastController.cs
+++ demoCourseEF/Controllers/WeatherForecastController.cs
@@ -1,33 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace demoCourseEF.Controllers
+namespace demoCourseEF.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    private readonly ILogger<WeatherForecastController> _logger;
+    private readonly IConfiguration _config;
+
+    public WeatherForecastController(
+        ILogger<WeatherForecastController> logger,
+        IConfiguration config)
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
+        _logger = logger;
+        this._config = config;
+    }
 
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
-
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
+    [HttpGet]
+    public IActionResult Get()
+    {
+        var conn = _config.GetConnectionString("DefaultConnection");
+        return Ok(conn);
     }
 }
--- demoCourseEF/Program.cs
+++ demoCourseEF/Program.cs
@@ -2,39 +2,35 @@
 using demoCourseEF.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace demoCourseEF
+namespace demoCourseEF;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+        var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+        // Add services to the container.
 
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        //builder.Services.AddSwaggerGen();
 
-            builder.Services.AddDbContext<ContosoUniversityContext>(
-        options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+        builder.Services.AddDbContext<ContosoUniversityContext>(
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-            var app = builder.Build();
+        var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+        //if (app.Environment.IsDevelopment())
+        //{
+        //    app.UseSwagger();
+        //    app.UseSwaggerUI();
+        //}
 
-            app.UseAuthorization();
+        app.UseAuthorization();
 
+        app.MapControllers();
 
-            app.MapControllers();
-
-            app.Run();
-        }
+        app.Run();
     }
 }
